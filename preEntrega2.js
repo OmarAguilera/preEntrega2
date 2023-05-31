2498544a7bc156dc7cd2d96f617e12080ed0ebd2
Omar S. Aguilera <omarsilva@politicas.unam.mx>
2023-05-31 13:23:33 -0600

    subir

--- preEntrega2.js
+++ preEntrega2.js
@@ -1,6 +1,6 @@
 
 console.log("Por favor ingrese algun alimento de la siguiente lista:");
-let opcionesDisponibles = ["manzana", "banana", "naranja", "yogurt", "pollo", "arroz", "lasagna", "hamburguesa", "pizza", "cremaDeCacahuate", "avena", "pollo", "proteina", "huevo"];
+let opcionesDisponibles = ["manzana", "lasagna", "hamburguesa", "pizza", "cremaDeCacahuate", "avena", "pollo", "proteina", "huevo"];
 /* for(i = 0; i < opcionesDisponibles.length; i++){
     console.log(opcionesDisponibles[i]);
 } */
@@ -30,20 +30,18 @@ let alimentosInfo = {
   huevo: 368
 };
 
-
-
 let alimentosRegistrados = [];
 let agregarAlimento = "";
 while(agregarAlimento !== "fin"){
-    agregarAlimento = prompt('Por favor abra la consola e ingrese un alimento de la lista de alimentos disponibles (se agregarán más), o escriba "fin" para salir');
+    agregarAlimento = prompt('Por favor abra la consola e ingrese un alimento de la lista de alimentos disponibles (se agregarán más), o escriba "fin" para salir. Si aun no ha abierto la consola primero escriba "fin", abra la consola y actualice.');
     if (agregarAlimento !== "fin"){
         alimentosRegistrados.push(agregarAlimento)};
-        console.log(alimentosRegistrados);
+        console.log("Nuevo alimento agregado: ",alimentosRegistrados);
 } 
 
 if(agregarAlimento == "fin"){
     for (let encontrarAlimento of alimentosRegistrados) {
-        console.log(encontrarAlimento);
+        console.log(encontrarAlimento + ": " + alimentosInfo[encontrarAlimento] + "kcal" );
         totalCalorias += alimentosInfo[encontrarAlimento]; // Los corchetes se usan para acceder a una propiedad utilizando una expresión evaludad dinámicamente. Cuando utilizamos la notación objeto.propiedad Javascript buscará una propiedad literalmente llamada como lo que pongamos después del punto. Por tanto esa notación solo nos sirve en caso de que conozcamos el nombre de la propiedad.
     }
 }
